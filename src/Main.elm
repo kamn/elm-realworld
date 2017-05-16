fb7be36272a12657bc90f34e678369b0ca4c7761
Samuel Miller <kamn.miller@gmail.com>
2017-05-15 23:27:40 -0600

    Got basic routing working

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,43 @@ import Html exposing (Html, text, div, button)
 import Html.Events exposing (onClick)
 import Views.Layout exposing (layout)
 import Views.Home exposing (home)
-import Navigation
+import Debug exposing (log)
+import Navigation exposing (Location)
+import UrlParser exposing (..)
 
-type alias Model = Int
+-- ROUTING
+
+type Route
+  = Home
+  | Settings
+  | NotFoundRoute
+
+route =
+  oneOf
+    [ map Home top
+    , map Settings (s "settings")
+    ]
+
+
+parseLocation: Location -> Route
+parseLocation location =
+  case (parseHash route location) of
+    Just r ->
+      r
+    Nothing ->
+      NotFoundRoute
+
+type alias Model = 
+  {
+    value: Int
+  , route: Route
+  }
+
 model : Model
 model =
-  0
+  { value = 0
+  , route = Home
+  }
 
 -- UPDATE
 
@@ -17,19 +48,28 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Increment ->
-      (model + 1, Cmd.none)
-    Decrement ->
-      (model + 1, Cmd.none)
-    _ ->
       (model, Cmd.none)
+    Decrement ->
+      (model, Cmd.none)
+    UrlChange loc ->
+      log loc.pathname
+      log loc.hash
+      log loc.href
+      ({model | route = parseLocation loc}, Cmd.none)
 
 view : Model -> Html Msg
 view model =
-  layout home
+  case model.route of
+    Home ->
+      layout home
+    Settings ->
+      div [] [text "Settings"]
+    _ ->
+      div [] [text "NotFound"]
 
 
 init location =
-  (model, Cmd.none)
+  ({model | route = parseLocation location}, Cmd.none)
 
 main =
   Navigation.program UrlChange
