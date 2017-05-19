174e7121d70119a6fc20fd40d2a7a94a43a6b89b
Samuel Miller <kamn.miller@gmail.com>
2017-05-18 23:16:13 -0600

    Fixed the issue with the footer! Also changed the ReadMe a little.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # elm-realworld
-Elm version of the realworld app
+[INCOMPLETE]
+
+> ### Elm codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld) spec and API.
+
+This codebase was created to demonstrate a fully fledged fullstack application built with Elm including CRUD operations, authentication, routing, pagination, and more.
 
 ## Purpose
 Even though a version for this app was made, I wanted to make my own and learn more about Elm.
--- src/Views/Layout.elm
+++ src/Views/Layout.elm
@@ -5,65 +5,71 @@ import Html.Attributes exposing (..)
 
 import Data.Msg exposing (Msg)
 
+stylesheet : String -> Html Msg
+stylesheet url =
+    let
+        tag = "link"
+        attrs =
+            [ attribute "rel"       "stylesheet"
+            , attribute "property"  "stylesheet"
+            , type_ "text/css"
+            , attribute "href"      url
+            ]
+        children = []
+    in 
+        node tag attrs children
+
+-- TODO Figure out how to do this outside of elm
+-- node "title" [] [ text "Conduit" ]
 layout : Html Msg -> Html Msg
 layout container =
-    div []
-          [ node "head" []
-              [ node "meta" [ charset "utf-8" ]
-                  []
-              , node "title" []
-                  [ text "Conduit" ]
-              , node "link" [ href "//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css", rel "stylesheet", type_ "text/css" ]
-                  []
-              , node "link" [ href "//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic", rel "stylesheet", type_ "text/css" ]
-                  []
-              , node "link" [ href "//demo.productionready.io/main.css", rel "stylesheet" ]
-                  []
-              ]
-          , div [class "page-frame"]
-                -- TODO - Highlight different components depending on where they are routing
-              [ nav [ class "navbar navbar-light" ]
-                  [ div [ class "container" ]
-                      [ a [ class "navbar-brand", href "#/" ]
-                          [ text "conduit" ]
-                      , ul [ class "nav navbar-nav pull-xs-right" ]
-                          [ li [ class "nav-item" ]
-                              [ a [ class "nav-link active", href "#/" ]
-                                  [ text "Home" ]
-                              ]
-                          , li [ class "nav-item" ]
-                              [ a [ class "nav-link", href "#/editor" ]
-                                  [ i [ class "ion-compose" ]
-                                      []
-                                  , text " New Post            "
-                                  ]
-                              ]
-                          , li [ class "nav-item" ]
-                              [ a [ class "nav-link", href "#/settings" ]
-                                  [ i [ class "ion-gear-a" ]
-                                      []
-                                  , text " Settings            "
-                                  ]
-                              ]
-                          , li [ class "nav-item" ]
-                              [ a [ class "nav-link", href "#/register" ]
-                                  [ text "Sign up" ]
-                              ]
-                          ]
-                      ]
-                  ]
-              , container
-              , footer []
-                  [ div [ class "container" ]
-                      [ a [ class "logo-font", href "/" ]
-                          [ text "conduit" ]
-                      , span [ class "attribution" ]
-                          [ text "An interactive learning project from "
-                          , a [ href "https://thinkster.io" ]
-                              [ text "Thinkster" ]
-                          , text ". Code & design licensed under MIT.        "
-                          ]
-                      ]
-                  ]
-              ]
-          ]
+    div [class "page-frame"]
+          [ stylesheet "//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          , stylesheet "//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          , stylesheet "//demo.productionready.io/main.css"
+          , node "style" [ type_ "text/css" ]
+            [ text "@import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro);html {  margin: 0;  height: auto;}" ]
+          , nav [ class "navbar navbar-light" ]
+                [ div [ class "container" ]
+                    [ a [ class "navbar-brand", href "#/" ]
+                        [ text "conduit" ]
+                    , ul [ class "nav navbar-nav pull-xs-right" ]
+                        [ li [ class "nav-item" ]
+                            [ a [ class "nav-link active", href "#/" ]
+                                [ text "Home" ]
+                            ]
+                        , li [ class "nav-item" ]
+                            [ a [ class "nav-link", href "#/editor" ]
+                                [ i [ class "ion-compose" ]
+                                    []
+                                , text " New Post            "
+                                ]
+                            ]
+                        , li [ class "nav-item" ]
+                            [ a [ class "nav-link", href "#/settings" ]
+                                [ i [ class "ion-gear-a" ]
+                                    []
+                                , text " Settings            "
+                                ]
+                            ]
+                        , li [ class "nav-item" ]
+                            [ a [ class "nav-link", href "#/register" ]
+                                [ text "Sign up" ]
+                            ]
+                        ]
+                    ]
+                ]
+            , container
+            , footer []
+                [ div [ class "container" ]
+                    [ a [ class "logo-font", href "/" ]
+                        [ text "conduit" ]
+                    , span [ class "attribution" ]
+                        [ text "An interactive learning project from "
+                        , a [ href "https://thinkster.io" ]
+                            [ text "Thinkster" ]
+                        , text ". Code & design licensed under MIT.        "
+                        ]
+                    ]
+                ]
+            ]
