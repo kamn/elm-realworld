f07ca366d21f586ae345873d68233cee314fa620
Samuel Miller <kamn.miller@gmail.com>
2017-05-29 21:11:47 -0600

    Added an authed get for the future

--- src/Utils.elm
+++ src/Utils.elm
@@ -3,7 +3,8 @@ module Utils exposing (..)
 import Html exposing (..)
 import Html.Events exposing (onWithOptions, Options, defaultOptions)
 import Json.Decode as Json
-
+import Http exposing (..)
+import Json.Decode as Decode
 
 -- TODO Move somewhere else?
 
@@ -11,3 +12,15 @@ import Json.Decode as Json
 onNoBubbleClick : msg -> Attribute msg
 onNoBubbleClick msg =
     onWithOptions "click" { defaultOptions | preventDefault = True } (Json.succeed msg)
+
+authedGet : String -> String -> Decode.Decoder a -> Request a
+authedGet url token decoder =
+  request
+    { method = "GET"
+    , headers = [Http.header "Authorization" token]
+    , url = url
+    , body = emptyBody
+    , expect = expectJson decoder
+    , timeout = Nothing
+    , withCredentials = False
+    }
